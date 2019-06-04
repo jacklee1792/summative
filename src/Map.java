4b3c841bd6d772cda038f5d516ca57dcf80c6495
Jack <jacklee1792@gmail.com>
2019-06-04 14:01:09 -0400

    Some changes

--- src/Map.java
+++ src/Map.java
@@ -2,10 +2,14 @@ import java.awt.*;
 import java.io.File;
 
 class Map {
-    /**  INSTANCE VARIABLES  **/
-    private MapComponent [][] map;
+    //Instance variables
+    private MapComponent [][][] map;
     private int rows, cols, squareSize;             // sizing parameters
     public int currentRow, currentCol;              // location of top left corner
+
+    //Static variables
+    final static int GROUND_LAYER = 0;
+    final static int ABOVE_GROUND_LAYER = 1;
 
     /**  CONSTRUCTORS  **/
     public Map (File loadFile) {
