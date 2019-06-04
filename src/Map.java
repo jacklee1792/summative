dbfd54baf611f1400d9ac3a370f6a03915cf5b45
Jack <jacklee1792@gmail.com>
2019-06-04 14:11:52 -0400

    Version for June 4 class

--- src/Map.java
+++ src/Map.java
@@ -1,11 +1,17 @@
+/**
+ * Map class
+ * In-depth documentation at:
+ *  https://docs.google.com/document/d/1Hu6XnzeBDa0TvPOOIS6JWaDXK1A8l10doPGio8VgK84/edit?usp=sharing
+ */
+
 import java.awt.*;
 import java.io.File;
 
 class Map {
     //Instance variables
     private MapComponent [][][] map;
-    private int rows, cols, squareSize;             // sizing parameters
-    public int currentRow, currentCol;              // location of top left corner
+    private int rows, cols, squareSize;
+    public int currentRow, currentCol;
 
     //Static variables
     final static int GROUND_LAYER = 0;
