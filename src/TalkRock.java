0c3fca133864ccbca1e62bc8e4aa443132ae1bcc
Bo <boyuankong@outlook.com>
2019-06-10 16:20:14 -0400

    minor edits

--- src/TalkRock.java
+++ src/TalkRock.java
@@ -1,3 +1,15 @@
 class TalkRock {
+    // Instance variables
+    public int stage;
+    public String[] lines;
 
+    // Constructor
+    public TalkRock(File loadFile){
+
+    }
+
+    // Methods
+    public String readLine(){
+        return lines[stage];
+    }
 }
