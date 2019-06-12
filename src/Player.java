4e9fdc23cacd9a307ee20c0fe66984a5210d5351
Positron9001 <simhonchourasia@gmail.com>
2019-06-12 15:12:14 -0400

    [2019-06-12] Added hitting, nonfunctional implementation of swapping inventory with 12345

--- src/Player.java
+++ src/Player.java
@@ -5,7 +5,9 @@ import java.nio.Buffer;
 import java.util.ArrayList;
 
 class Player extends Entity {
-    /**  INSTANCE VARIABLES  **/
+    /**
+     * INSTANCE VARIABLES
+     **/
     private String name;
     private double hungerFactor;
     private ArrayList<Item> inventory;
@@ -33,16 +35,18 @@ class Player extends Entity {
     public int getSelectedIndex() {
         return selectedIndex;
     }
+
     public void setSelectedIndex(int index) {
         selectedIndex = index;
     }
+
     public ArrayList<Item> getInventory() {
         return inventory;
     }
 
     public static void importTextures() throws IOException { //import textures
         texture = new BufferedImage[8];
-        for(int i = 1; i <= 8; i++) {
+        for (int i = 1; i <= 8; i++) {
             texture[i - 1] = ImageIO.read(Player.class.getResourceAsStream("_PLYR" + i + ".png"));
         }
     }
@@ -52,16 +56,22 @@ class Player extends Entity {
     -Better interaction logic -> interacting with both ground layer and item layer within player
      */
     public void interact(MapComponent m) {
-        if(m.getMapComponentID() == MapComponent.SMALL_TREE && inventory.size() < inventoryCap) {
+        if (m.getMapComponentID() == MapComponent.SMALL_TREE && inventory.size() < inventoryCap) {
             inventory.add(new Item(Item.STICK));
             System.out.println("Interaction with small tree detected");
-        } else if(m.getMapComponentID() == MapComponent.SMALL_BUSH && inventory.size() < inventoryCap) {
+        } else if (m.getMapComponentID() == MapComponent.SMALL_BUSH && inventory.size() < inventoryCap) {
             System.out.println("Interaction with small bush detected");
+        } else if (m.getMapComponentID() == MapComponent.ROCKS && inventory.size() < inventoryCap) {
+            System.out.println("Interaction with rocks detected");
         }
-        for(Item i : inventory) {
+        for (Item i : inventory) {
             System.out.print(i.getItemID());
         }
         System.out.println();
+    }
+
+    public void dropItem(){
+        inventory.remove(selectedIndex);
     }
 
 }
