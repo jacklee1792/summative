576ac6a9b7b8951384d02d9fe720994bfbd844bb
Positron9001 <simhonchourasia@gmail.com>
2019-06-14 22:08:52 -0400

    [2019-06-14] Added more items and fixed item sprites

--- src/Item.java
+++ src/Item.java
@@ -6,25 +6,71 @@ class Item {
 
     private int I_ID;
 
+    // UPDATE THIS WHENEVER YOU ADD A NEW ITEM
     final static int numItems = 3;
+
     final static int NULL = 0;
     final static int STICK = 1;
     final static int SWORD = 2;
+    final static int KNIFE = 3;
+    final static int SLINGSHOT = 4;
 
+    final static int APPLE = 5;
+    final static int BERRY = 6;
+    final static int MEAT = 7;
+
+    // DAMAGE OF EACH ITEM
     final static int NULL_DAMAGE = 0;
     final static int STICK_DAMAGE = 1;
     final static int SWORD_DAMAGE = 4;
+    final static int KNIFE_DAMAGE = 10;
+    final static int SLINGSHOT_DAMAGE = 3;
+
+    final static int APPLE_DAMAGE = 0;
+    final static int BERRY_DAMAGE = 0;
+    final static int MEAT_DAMAGE = 0;
+
+    // RANGE OF EACH ITEM
+    final static int NULL_RANGE = 2;
+    final static int STICK_RANGE = 3;
+    final static int SWORD_RANGE = 3;
+    final static int KNIFE_RANGE = 1;
+    final static int SLINGSHOT_RANGE = 7;
+
+    final static int APPLE_RANGE = 1;
+    final static int BERRY_RANGE = 1;
+    final static int MEAT_RANGE = 1;
 
     private int damage;
+    private int range;
 
     public Item(int ItemID) {
         I_ID = ItemID;
-        if (I_ID == NULL)
+        if (I_ID == NULL) {
             damage = NULL_DAMAGE;
-        else if (I_ID == STICK)
+            range = NULL_RANGE;
+        } else if (I_ID == STICK) {
             damage = STICK_DAMAGE;
-        else if (I_ID == SWORD)
+            range = STICK_RANGE;
+        } else if (I_ID == SWORD) {
             damage = SWORD_DAMAGE;
+            range = SWORD_RANGE;
+        } else if (I_ID == KNIFE) {
+            damage = KNIFE_DAMAGE;
+            range = KNIFE_RANGE;
+        } else if (I_ID == SLINGSHOT){
+            damage = SLINGSHOT_DAMAGE;
+            range = SLINGSHOT_RANGE;
+        } else if (I_ID == APPLE){
+            damage = APPLE_DAMAGE;
+            range = APPLE_RANGE;
+        } else if (I_ID == BERRY){
+            damage = BERRY_DAMAGE;
+            range = BERRY_RANGE;
+        } else if (I_ID == MEAT){
+            damage = MEAT_DAMAGE;
+            range = MEAT_RANGE;
+        }
     }
 
     public int getItemID() {
@@ -42,4 +88,5 @@ class Item {
 
     public int getDamage() {return damage; }
 
+    public int getRange() {return range; }
 }
